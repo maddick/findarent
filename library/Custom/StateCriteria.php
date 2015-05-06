c7604a76f96b95452e759b444c095f613919d7c5
maddick <mike.matovic@gmail.com>
2015-05-05 21:21:32 -0400

    added state criteria

---
+++ library/Custom/StateCriteria.php
@@ -0,0 +1,22 @@
+<?php
+
+class Custom_StateCriteria extends Custom_AbstractCriteria
+{
+    public function __construct($state)
+    {
+        parent::__construct($state);
+    }
+
+    protected function _validate()
+    {
+        if ( empty( $this->_criteriaValue ) ) {
+            $this->_validationErrors[] = 'state was unspecified';
+        } else {
+            $stateRegEx = '/$[a-zA-Z]{2}^/';
+            $stateValidator = new Zend_Validate_Regex( array( 'pattern' => $stateRegEx ) );
+            if ( !$stateValidator->isValid($this->_criteriaValue) ) {
+                $this->_validationErrors[] = 'not a valid state value';
+            }
+        }
+    }
+}
