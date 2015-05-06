7f41cf06573aa8ad1cfa8029a7b0cf38decb29d1
maddick <mike.matovic@gmail.com>
2015-05-05 21:41:33 -0400

    added featured communities controller

---
+++ application/modules/communities/controllers/FeaturedCommunitiesController.php
@@ -0,0 +1,34 @@
+<?php
+
+class Communities_FeaturedCommunitiesController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        $this->_helper->viewRenderer->setNoRender(true);
+    }
+
+    public function GetCommunityCitiesByStateAction()
+    {
+        $state = $this->getRequest()->getParam('state');
+        $stateCriteria = new Custom_StateCriteria( $state );
+        $featuredCommunitiesModel = new Communities_Models_FeaturedCommunities();
+        $featuredCommunitiesModel->setStateCriteria($stateCriteria);
+        $citiesByState = $featuredCommunitiesModel->getCommunityCitiesByState();
+
+        if ( $citiesByState['result'] === 'success' ) {
+            if ( empty( $citiesByState['communities'] ) ) {
+                $this->getResponse()->setHttpResponseCode(404);
+            } else {
+                $this->getResponse()->setHttpResponseCode(200);
+            }
+        } else {
+            if ( $citiesByState['result'] === 'server error' ) {
+                $this->getResponse()->setHttpResponseCode(500);
+            } else {
+                $this->getResponse()->setHttpResponseCode(400);
+            }
+        }
+
+        return $this->_helper->json->sendJson( $citiesByState, false, true );
+    }
+}
