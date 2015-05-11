7b62966e88c71b007c8479ff1e571df39373fa38
maddick <mike.matovic@gmail.com>
2015-05-10 23:21:40 -0400

    rebuilt listingSearchController
    listing search controller is now dependant on url's and routes as to
    allow for bookmarking of searches.

--- public/app/controllers/listingSearchController.js
+++ public/app/controllers/listingSearchController.js
@@ -1,30 +1,31 @@
 angular
     .module('app')
     .controller('listingSearchController',['$scope', '$routeParams', 'ListingSearch', function($scope,$routeParams,ListingSearch){
-        //$scope.searchListings = function() {
+        var searchParams = {};
+        var isCityState = $routeParams.cityState !== undefined;
+        var cityState = '';
+        var zipCode = '';
 
-            var searchParams = {};
-            var isCityState = $routeParams.cityState !== undefined;
-            var cityState = '';
-            var zipCode = '';
+        if ( isCityState ) {
+            cityState = $routeParams.cityState;
+            searchParams['cityState'] = cityState;
+        } else {
+            zipCode = $routeParams.zipCode;
+            searchParams['zipCode'] = zipCode;
+        }
 
-            if ( isCityState ) {
-                cityState = $routeParams.cityState;
-                searchParams['cityState'] = cityState;
-            } else {
-                zipCode = $routeParams.zipCode;
-                searchParams['zipCode'] = zipCode;
-            }
+        if ( $routeParams.numberOfBedrooms !== undefined) {
+            searchParams['numberOfBedrooms'] = $routeParams.numberOfBedrooms;
+        }
 
-            if ( $routeParams.numberOfBedrooms !== undefined) {
-                searchParams['numberOfBedrooms'] = $routeParams.numberOfBedrooms;
-            }
+        console.log(searchParams);
 
-            console.log(searchParams);
-
-            var promise = ListingSearch.get(searchParams);
-            promise.then(function(response){
-                $scope.listings = response.data;
-            });
-        //}
+        var promise = ListingSearch.get(searchParams);
+        promise.then(
+        function(response){
+            $scope.listings = response.data;
+        },
+        function(response){
+            $scope.listings = response.data;
+        });
     }]);
