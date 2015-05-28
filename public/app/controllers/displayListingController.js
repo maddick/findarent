e4f0ff7661d1e25d98d4bf73f5feb58101398589
maddick <mike.matovic@gmail.com>
2015-05-27 23:07:35 -0400

    added loading screen for display listings

--- public/app/controllers/displayListingController.js
+++ public/app/controllers/displayListingController.js
@@ -2,14 +2,25 @@ angular
     .module('app')
     .controller('displayListingController',['$scope','$routeParams','ListingRest','$location',function($scope,$routeParams,ListingRest,$location){
         var listingId = ($routeParams.listingId === undefined ) ? '' : $routeParams.listingId;
+
+        //show a loading screen
+        $('#display-listing-loading').fadeIn();
+
         var promise = ListingRest.getListingById( listingId );
         promise.then(
             function(response){
                 $scope.listing = response.data.listing[0];
+                $scope.listing.result = response.data.result;
+                $scope.listing.reasons = response.data.reasons;
                 $scope.goToAddress = $scope.listing.Address + ',' + $scope.listing.City + ',' + $scope.listing.State;
+                $('#display-listing-loading').fadeOut(400, function(){
+                    $('#display-listing-success').fadeIn();
+                });
                 console.log($scope.listing);
             },
             function(response){
+                $('#display-listing-loading').fadeOut();
+                $scope.listing = response.data.listing[0];
                 console.log(response);
             }
         );
