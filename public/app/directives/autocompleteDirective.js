4b281632094063b216957a289d1bed5f17675dcf
maddick <mike.matovic@gmail.com>
2015-06-03 00:28:08 -0400

    restructured code
    took expensive list editing out of loop

--- public/app/directives/autocompleteDirective.js
+++ public/app/directives/autocompleteDirective.js
@@ -9,15 +9,14 @@ angular
                     .then(
                         function(value){
                             scope.suggestedValues = value.data['cities-and-zip-codes'];
-
+                            var values = $.map( scope.suggestedValues, function(item){
+                                return {
+                                    label : item.city_name + ', ' + item.state_abbr + ' ' + item.zip_code,
+                                    value : item.city_name + ', ' + item.state_abbr + ' ' + item.zip_code
+                                }
+                            });
                             $(element).autocomplete({
                                 source : function(request, response){
-                                    var values = $.map( scope.suggestedValues, function(item){
-                                        return {
-                                            label : item.city_name + ', ' + item.state_abbr + ' ' + item.zip_code,
-                                            value : item.city_name + ', ' + item.state_abbr + ' ' + item.zip_code
-                                        }
-                                    });
                                     var results = $.ui.autocomplete.filter(values, request.term);
                                     response(results.slice(0,10));
                                 },
