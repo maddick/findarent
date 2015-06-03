2352bd9b2c0826a28c0ea6e4efbc50938a276860
maddick <mike.matovic@gmail.com>
2015-06-02 21:14:13 -0400

    fixed directive

--- public/app/directives/autocompleteDirective.js
+++ public/app/directives/autocompleteDirective.js
@@ -1,13 +1,15 @@
-app
-    .directive('autocomplete',['autocompleteDataService',function(autocompleteDataService){
+angular
+    .module('app')
+    .directive('autocomplete',function(){
         return{
             restrict : 'A',
             link : function(scope, element, attrs){
                 $(element).autocomplete({
                     source : function(request, response){
-                        var promise = autocompleteDataService.getSuggestions($(element).val());
-                        promise.then(
-                            function(data){
+                        $.ajax({
+                            type: 'GET',
+                            url: 'http://192.168.0.101:8080/listing/search/get-autocomplete-suggestions/autocomplete-data/' + $('#cityStateOrZip').val(),
+                            success: function (data) {
                                 response($.map( data.suggestedValues, function(item){
                                     return {
                                         label : item.Location,
@@ -15,12 +17,16 @@ app
                                     }
                                 }));
                             },
-                            function(data){
-                                //error handling
+                            error : function (textStatus) {
+                                console.log(textStatus);
                             }
-                        );
+                        });
+                    },
+                    select : function(event, ui){
+                        scope.listingSearchParams.cityStateOrZip = ui.item.value;
+                        $(element).val(ui.item.value);
                     }
                 });
             }
         }
-    }]);
+    });
