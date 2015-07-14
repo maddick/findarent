d16703a91f9e6b309a53206c7614c99a40b95659
maddick <mike.matovic@gmail.com>
2015-07-14 18:15:52 -0400

    updated validation and sending
    form now sends. Works currently for listing emails

--- findarentjs/app/controllers/contactFormController.js
+++ findarentjs/app/controllers/contactFormController.js
@@ -1,30 +1,53 @@
 angular
     .module('app')
-    .controller('contactFormController',['$scope','$routeParams','$location','ListingRest',function($scope,$routeParams,$location,ListingRest){
+    .controller('contactFormController',['$scope','$routeParams','$location','$http','ListingRest',function($scope,$routeParams,$location,$http,ListingRest){
         var path = $location.path();
-        var search = $location.search();
+        var listingId = ($routeParams.listingId !== undefined) ? $routeParams.listingId : '';
         var forListing = path.indexOf('/Contact-Owner') != -1;
         var forBroker = path.indexOf('/Contact-Broker') != -1;
         var forCommunity = path.indexOf('/Contact-Community') != -1;
 
-        $scope.questions = [
-            {
-                value: 'Visit',
-                label: 'Please call/email me to set up a visit to your apartment(s)'
-            },
-            {
-                value: 'Pets',
-                label: 'Do you allow pets?'
-            },
-            {
-                value: 'Availability',
-                label: 'When is the apartment available?'
-            },
-            {
-                value: 'Questions',
-                label: 'Other - Please call me so that I can ask questions'
-            }
-        ];
+        if (forListing || forCommunity) {
+            $scope.questions = [
+                {
+                    value: 'Visit',
+                    label: 'Please call/email me to set up a visit to your apartment(s)'
+                },
+                {
+                    value: 'Pets',
+                    label: 'Do you allow pets?'
+                },
+                {
+                    value: 'Availability',
+                    label: 'When is the apartment available?'
+                },
+                {
+                    value: 'Questions',
+                    label: 'Other - Please call me so that I can ask questions'
+                }
+            ];
+        }
+
+        if (forBroker) {
+            $scope.questions = [
+                {
+                    value: 'Pets',
+                    label: 'Do any or the apartments you work with allow pets?'
+                },
+                {
+                    value: 'Schedule',
+                    label: 'Can your properties accommodate my moving schedule?'
+                },
+                {
+                    value: 'Parking',
+                    label: 'Do any of your properties have parking?'
+                },
+                {
+                    value: 'Questions',
+                    label: 'Please call so we can discuss my needs'
+                }
+            ];
+        }
 
         $scope.message = {};
         $scope.message.subject = 'RE: ';
@@ -43,32 +66,30 @@ angular
         $scope.validation = {};
         $scope.validation.senderEmailNotValid = true;
         $scope.validation.formIsValid = false;
+        $scope.validation.senderFirstNameValid = false;
+        $scope.validation.senderAdditionalMessageValid = false;
         $scope.validation.validateInfo = function() {
-            var senderFirstNameValid = $scope.message.senderFirstName !== undefined &&
+            console.log('called');
+            $scope.validation.senderFirstNameValid = $scope.message.senderFirstName !== undefined &&
                 $scope.message.senderFirstName !== null &&
                 $scope.message.senderFirstName !== '';
 
-            var senderAdditionalMessageValid = ($scope.additionalField.isRequired) ? $scope.message.senderAdditionalMessage !== undefined &&
+            $scope.validation.senderAdditionalMessageValid = ($scope.additionalField.isRequired) ? $scope.message.senderAdditionalMessage !== undefined &&
                 $scope.message.senderAdditionalMessage !== null &&
                 $scope.message.senderAdditionalMessage !== '' : true;
 
-            console.log(senderFirstNameValid);
-            console.log(senderAdditionalMessageValid);
-            var temp = $scope.validation.senderEmailNotValid;
-            console.log(temp);
-
-            return (senderFirstNameValid && senderAdditionalMessageValid && !$scope.validation.senderEmailNotValid);
+            return ($scope.validation.senderFirstNameValid && $scope.validation.senderAdditionalMessageValid && !$scope.validation.senderEmailNotValid);
 
         };
 
         if (forListing) {
             //need to get the listing and send it along
-            var promise = ListingRest.getListingById(search['listing-id']);
+            var promise = ListingRest.getListingById(listingId);
 
             promise.then(
                 function(response){
-                    $scope.message.listing = response.data.listing;
-                    $scope.message.subject += $scope.message.listing.MarketingMessage;
+                    $scope.message.listing = response.data.listing[0];
+                    $scope.message.subject += $scope.message.listing.Headline;
                 },
                 function(response){
                     //TODO: set an error message
@@ -105,15 +126,15 @@ angular
         };
 
         $scope.submit = function(){
-            var temp = $scope.message.MessageObj;
+            var temp = $scope.validation.validateInfo();
+            console.log(temp);
 
             var payload = {};
 
             if ( $scope.validation.validateInfo() ) {
                 $scope.message.senderMessage = $scope.message.MessageObj.label;
-                payload.senderMessage = $scope.message.senderMessage;
-                payload.senderFirstName = $scope.message.senderFirstName;
-                payload.senderLastName = $scope.message.senderLastName;
+                payload.senderMessage = $scope.message.senderMessage + ' : ' + $scope.message.senderAdditionalMessage;
+                payload.senderName = $scope.message.senderFirstName + ' ' + $scope.message.senderLastName;
                 payload.senderEmail = $scope.message.senderEmail;
                 payload.Phone = $scope.message.Phone;
 
@@ -125,9 +146,30 @@ angular
                 } else if (forCommunity) {
 
                 }
+
+                var promise  = $http(
+                    {
+                        method: 'POST',
+                        url: 'http://192.168.0.101:8080/communication/message/send-email-to-owner/',
+                        headers : { 'Content-Type' : 'application/json' },
+                        data: payload
+                    }
+                );
+
+                promise.then(
+                    function(response){
+                        console.log('success');
+                        console.log(response);
+                    },
+                    function(response) {
+                        console.log('error');
+                        console.log(response);
+                    }
+                );
             }
 
             console.log(payload);
+            console.log(payload.senderEmail);
 
         };
     }]);
