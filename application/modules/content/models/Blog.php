bf1392bb5e44805f08c3c44d320e274f82b844b8
maddick <mike.matovic@gmail.com>
2015-07-23 20:56:43 -0400

    utf8 encoding bug fixed

--- application/modules/content/models/Blog.php
+++ application/modules/content/models/Blog.php
@@ -12,12 +12,16 @@ class Content_Model_Blog
             $sql =
                 'SELECT *
                 FROM wp_posts
-                WHERE post_status = \'publish\'';
+                WHERE post_status = \'publish\'
+                AND post_type = \'post\'';
 
             $stmt = $db->prepare($sql);
             $stmt->execute();
             $this->_results['result'] = 'success';
             $this->_results['blogs'] = $stmt->fetchAll();
+            foreach( $this->_results['blogs'] as $key => $value ) {
+                $this->_results['blogs'][$key]['post_content'] = json_encode(utf8_encode($this->_results['blogs'][$key]['post_content']));
+            }
         } catch ( Exception $e ) {
             $this->_results['result'] = 'server error';
             $this->_results['reasons'] = $e->getMessage();
